2988ee8830567ad1128f363ea39b4909121f728c
unknown <alyssonja23@gmail.com>
2022-03-16 06:35:17 -0300

    feature: configurando sequelize

---
+++ src/database/config/config.ts
@@ -0,0 +1,11 @@
+const database = {
+
+    username: "root",
+    password: null,
+    database: "class-manager_development",
+    host: "127.0.0.1",
+    dialect: "sqlite3"
+  
+}
+
+export default database;
