0709069245ed3b5f4bc943236add29a9befbd718
unknown <alyssonja23@gmail.com>
2022-03-16 02:54:33 -0300

    feature: configurando ambiente de testes com jest

---
+++ __tests__/units/users.test.ts
@@ -0,0 +1,22 @@
+
+describe('Register',()=>{
+    it('should create an new user and receive this user with JWT token',()=>{
+
+    })
+});
+
+describe('Authenticate', ()=>{
+    it('should receive JWT token when authenticated with valid credentials',()=>{
+
+    })
+});
+
+describe('Admin Management',()=>{
+    it('',()=>{})
+});
+
+describe('Default Management',()=>{
+    it('',()=>{})
+});
+
+
--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,22 @@
 import { Router } from 'express'
 
 const router = Router();
+router.post('/users/register',(req,res)=> res.status(201).send());
+router.post('/users/login',(req,res)=> res.status(200).send());
+router.get('/users/:id',(req,res)=> res.status(200).send());
+router.put('/users/:id',(req,res)=> res.status(204).send());
+router.post('/users/:id/delete',(req,res)=> res.status(204).send());
 
-router.post('/users',(req,res)=> res.status(200).send());
-router.post('/users/register',(req,res)=> res.status(200).send());
-
-router.post('/classes',(req,res)=> res.status(200).send());
+router.post('/classes',(req,res)=> res.status(201).send());
 router.get('/classes',(req,res)=> res.status(200).send());
 router.get('/classes/:id',(req,res)=> res.status(200).send());
-router.put('/classes',(req,res)=> res.status(200).send());
-router.delete('/classes/:id',(req,res)=> res.status(200).send());
+router.put('/classes/:id',(req,res)=> res.status(204).send());
+router.delete('/classes/:id',(req,res)=> res.status(204).send());
 
-router.post('/classes/:id_class/comments',(req,res)=> res.status(200).send());
+router.post('/classes/:id_class/comments',(req,res)=> res.status(201).send());
 router.get('/classes/:id_class/comments',(req,res)=> res.status(200).send());
-router.delete('/classes/id_class/comments/:id',(req,res)=> res.status(200).send());
+router.put('/classes/id_class/comments/:id',(req,res)=> res.status(204).send());
+router.delete('/classes/id_class/comments/:id',(req,res)=> res.status(204).send());
 
 
 
